77bfc41f35e301bfccdb719658006bbecbb61ca3
Evan <calliaevan12@gmail.com>
2016-08-18 13:46:50 -0700

    updated setting for server

--- bbOne/settings.py
+++ bbOne/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'bbOne.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bbone', # This name must be lowercase!
+        'USER': 'evcallia', # you can put whatever you want here
+        'PASSWORD': 'enter', # you can put whatever you want here
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
